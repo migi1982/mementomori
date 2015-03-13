a79a91e13c05e8a0dca2b0d20ccf82fb61b8418f
migitera takanobu <t.EDEN.m@gmail.com>
2015-03-13 22:57:53 +0900

    mock

--- modules/common/services.js
+++ modules/common/services.js
@@ -1,4 +1,38 @@
 (function() {
-  angular.module('services', ['ngRoute']);
+  var dataService, util;
+
+  util = function() {
+    var rnd;
+    rnd = function(min, max) {
+      var num;
+      num = Math.random() * max + min;
+      num = num << 0;
+      return num;
+    };
+    return {
+      rnd: rnd
+    };
+  };
+
+  dataService = [
+    '$http', '$q', function($http, $q) {
+      return function(str) {
+        var delay;
+        delay = $q.defer();
+        $http.get('data/' + str + '.json').success(function(data, status, headers, config) {
+          delay.resolve(data);
+        }).error(function(data, status, headers, config) {
+          console.log('error!!');
+          console.log(data);
+          console.log(status);
+          console.log(headers);
+          console.log(config);
+        });
+        return delay.promise;
+      };
+    }
+  ];
+
+  angular.module('services', ['ngRoute']).service('dataService', dataService).service('util', util);
 
 }).call(this);
