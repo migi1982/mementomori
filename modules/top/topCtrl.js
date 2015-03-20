74c40ec22dc33ea7056f2774680f2b81ffcac269
migitera takanobu <t.EDEN.m@gmail.com>
2015-03-20 20:51:26 +0900

    fix

--- modules/top/topCtrl.js
+++ modules/top/topCtrl.js
@@ -1,20 +1,22 @@
 (function() {
-  var topCtrl;
+  var topCtrl, topService;
 
   topCtrl = [
-    '$scope', 'dataService', 'd3Service', 'showService', 'numberFilter', function($scope, dataService, d3Service, showService, numberFilter) {
-      var c3, d3, data10, data20, setDonut;
-      showService.toggle('topQ', true);
-      showService.toggle('topA', false);
+    '$scope', 'dataService', 'showService', 'topService', function($scope, dataService, showService, topService) {
+      var data10, data20;
+      showService.show('topQ');
+      showService.hide('topA');
       data10 = data20 = null;
-      d3 = d3Service.d3;
-      c3 = d3Service.c3;
-      dataService('10').then(function(res) {
+      dataService('deathRate').then(function(res) {
         data10 = res;
       });
-      dataService('20').then(function(res) {
+      dataService('deathRank').then(function(res) {
         data20 = res;
       });
+      $scope.showQ = function() {
+        showService.hide('topA');
+        return showService.show('topQ');
+      };
       $scope.setData = function() {
         var index10, index20;
         if ($scope.myAge >= 0 && $scope.mySex) {
@@ -29,14 +31,22 @@
           }
           $scope.rateData = data10[index10];
           $scope.rankData = data20[index20];
-          setDonut($scope.rankData[$scope.mySex]);
-          $scope.showDiv = 'a';
-          showService.toggle('nav', true);
-          showService.toggle('amazon', true);
-          showService.toggle('topQ');
-          showService.toggle('topA');
+          topService.setDonut($scope.rankData[$scope.mySex]);
+          showService.show('nav');
+          showService.show('amazon');
+          showService.hide('topQ');
+          showService.show('topA');
         }
       };
+      $scope.openTweetWindow = function() {
+        topService.openTweetWindow($scope.rateData.num[$scope.mySex], $scope.rankData[$scope.mySex][0].name);
+      };
+    }
+  ];
+
+  topService = [
+    'numberFilter', function(numberFilter) {
+      var openTweetWindow, setDonut;
       setDonut = function(data) {
         var chart, i, item, len, newData;
         newData = [];
@@ -60,23 +70,21 @@
           }
         });
       };
-      $scope.showQ = function() {
-        showService.toggle('topA');
-        return showService.toggle('topQ');
-      };
-      $scope.open_tweet_window = function() {
-        var cause, number, text, url;
-        number = $scope.rateData.num[$scope.mySex];
+      openTweetWindow = function(number, cause) {
+        var text, url;
         number = numberFilter(number);
-        cause = $scope.rankData[$scope.mySex][0].name;
         text = "私は今年、1/" + number + "の確率で死にます。死因はおそらく" + cause + "です。 #HowWeDie";
         text = encodeURIComponent(text);
         url = 'https://twitter.com/share?text=' + text;
         window.open(url, 'scrollbars=yes,width=500,height=300,');
       };
+      return {
+        setDonut: setDonut,
+        openTweetWindow: openTweetWindow
+      };
     }
   ];
 
-  angular.module('app').controller('topCtrl', topCtrl);
+  angular.module('app').controller('topCtrl', topCtrl).service('topService', topService);
 
 }).call(this);
